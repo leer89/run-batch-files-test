b085b58cbaa32300ad1fa1e552565a89cf56d694
leer89 <leer89@gmail.com>
2017-09-05 16:56:08 -0400

    initial commit

--- run-batch-files-test/Form1.Designer.cs
+++ run-batch-files-test/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "add batch locations below"});
+            this.listBox1.Location = new System.Drawing.Point(25, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(226, 56);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "run scripts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(117, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "debug text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "batch runner X-TREME";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- run-batch-files-test/Form1.cs
+++ run-batch-files-test/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,79 @@ namespace run_batch_files_test
 {
     public partial class Form1 : Form
     {
+        // create new list
+        List<string> _items = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+
+            // gets the data source for these items
+            listBox1.DataSource = _items;
+        }
+
+        // run scripts button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // get string from listbox at selected index
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+
+            label1.Text = text;
+
+            // handles starting up stupid command prompt and stuff
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c" + text);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            // reads error to log
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.Start();
+
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+        }
+
+        // add scripts location button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // add button was clicked
+            button2.Enabled = true;
+
+            _items.Add(textBox1.Text);
+
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
+        }
+
+        // remove scripts location button at selected index
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // check to see if collections exists
+            if (listBox1.Items.Count == 0)
+            {
+                button2.Enabled = false;
+            }
+
+            // create new integer object selected index
+            int selectedIndex = listBox1.SelectedIndex;
+
+            try
+            {
+                // remove item in the list
+                _items.RemoveAt(selectedIndex);
+            }
+            catch
+            {
+
+            }
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
         }
     }
 }
